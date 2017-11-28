08ccef76ee999fd1344b7fa6d4f37d90b73e7b06
lochenger <lochenger@gmail.com>
2017-11-27 21:49:40 -0800

    Final

---
+++ Data Structures/Linked Lists/Insert a Node at the Tail of a Linked List.java
@@ -0,0 +1,25 @@
+/*
+  Insert Node at the end of a linked list 
+  head pointer input could be NULL as well for empty list
+  Node is defined as 
+  class Node {
+     int data;
+     Node next;
+  }
+*/
+Node Insert(Node head,int data) {
+    Node n = new Node();
+    n.data = data;
+    n.next = null;
+    
+    if (head == null) {
+        return n;
+    } else {
+        Node curr = head;
+        while (curr.next != null) {
+            curr = curr.next;
+        }
+        curr.next = n;
+        return head;
+    }
+}
